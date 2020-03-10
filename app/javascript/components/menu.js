77ce439e533d03c52a24ee582d4bcb849ff986a7
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-03-10 16:12:04 +0200

    added border to current item in menu

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -13,8 +13,28 @@ const menuToggler = () => {
 };
 
 const menuSelector = () => {
-
   const url = window.location.href
+  const final = url.substr(url.lastIndexOf('/') + 1);
+  const arr = ['#', 'projects', 'about', 'avi', 'articles', 'contact'];
+
+  // const checker = value => {
+  //   !url.some(element => value.includes(element));
+  // }
+  // const currentUrl = arr.filter(checker)
+  let urlPath;
+  arr.forEach(path => {
+    if (final.indexOf(path) >= 0) {
+      urlPath = path;
+    };
+  });
+  if (urlPath === undefined) {
+    const item = document.getElementById("item-home");
+    item.classList.add('selected');
+  } else {
+    const item = document.getElementById(`item-${urlPath}`);
+    item.classList.add('selected');
+  }
+
 }
 
 export { menuToggler, menuSelector };
